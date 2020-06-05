f9cb770f29f7ddf3204ac64698ba4b64c1150a03
Victor <development@shodimeji.com>
2020-06-05 17:40:37 +0100

    initial code commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MemberAddComponent } from './member-add/member-add.component';
+import { MemberGetComponent } from './member-get/member-get.component';
+import { MemberEditComponent } from './member-edit/member-edit.component';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MemberAddComponent,
+    MemberGetComponent,
+    MemberEditComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, SlimLoadingBarModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
